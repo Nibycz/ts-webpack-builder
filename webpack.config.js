787823b664c665897ccb12eb56e5514c439784f7
Nibycz <emrah.esati@web.de>
2022-12-06 01:37:43 +0100

    added copy-webpack-plugin for txt files

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,41 @@
-const path = require( 'path' );
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    // bundling mode
-    mode: 'production',
-    watch: true,
-    // entry files
-    entry: './src/index.ts',
-    // output bundles (location)
-    output: {
-        path: path.resolve( __dirname, 'dist' ),
-        filename: 'main.js',
-    },
-    // file resolutions
-    resolve: {
-        extensions: [ '.ts', '.js' ],
-    },
-    // loaders
-    module: {
-        rules: [
-            {
-                test: /\.tsx?/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            }
-        ]
-    }
+  target: 'node',
+  // bundling mode
+  mode: 'production',
+  watch: true,
+  // entry files
+  entry: './src/index.ts',
+  // output bundles (location)
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  // file resolutions
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  // loaders
+  module: {
+    rules: [
+      {
+        test: /\.tsx?/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    //   {
+    //     test: /\.txt$/i,
+    //     use: 'raw-loader',
+    //   },
+    ],
+  },
+  // plugins
+  plugins: [
+    // copy .txt files from src to dist
+    new CopyPlugin({
+        patterns: [{ from: 'src/**/*.txt',  to:path.resolve(__dirname, 'dist', '[name][ext]'), }],
+    }),
+  ],
 };
